7795b13abcf94a29b54b5c4a8804524503b62a51
GokulVasan <gokulvas@gmail.com>
2018-02-08 16:07:26 +0100

    for BBB from 1.5G -> 6.5G files

--- files.h
+++ files.h
@@ -1,20 +1,22 @@
 /* Auto Generating test files\n
-Discarding device blocks:   4096/358400             done                            
-Creating filesystem with 358400 4k blocks and 89760 inodes
-Filesystem UUID: 7c9d37ad-53bf-424c-8d9e-cc0b6a9d6636
+Discarding device blocks:    4096/1587200               done                            
+Creating filesystem with 1587200 4k blocks and 397488 inodes
+Filesystem UUID: a28dd175-9581-42d9-aa69-97c3d745d978
 Superblock backups stored on blocks: 
-	32768, 98304, 163840, 229376, 294912
+	32768, 98304, 163840, 229376, 294912, 819200, 884736
 
-Allocating group tables:  0/11     done                            
-Writing inode tables:  0/11     done                            
-Creating journal (8192 blocks): done
-Writing superblocks and filesystem accounting information:  0/11     done
+Allocating group tables:  0/49     done                            
+Writing inode tables:  0/49     done                            
+Creating journal (32768 blocks): done
+Writing superblocks and filesystem accounting information:  0/49     done
 
 */
 typedef enum file_len {
 	 Itema ,
 	 Itemb ,
 	 Itemc ,
+	 Itemd ,
+	 Iteme ,
 	 max
 }file_len_t;
 static file_path file_Itema[] = { 
@@ -179,8 +181,118 @@ static file_path file_Itemc[] = {
 	 {0, NULL},
 };
 
+static file_path file_Itemd[] = { 
+	 {0, PATH(32M_1.txt)},
+	 {0, PATH(32M_2.txt)},
+	 {0, PATH(32M_3.txt)},
+	 {0, PATH(32M_4.txt)},
+	 {0, PATH(32M_5.txt)},
+	 {0, PATH(32M_6.txt)},
+	 {0, PATH(32M_7.txt)},
+	 {0, PATH(32M_8.txt)},
+	 {0, PATH(32M_9.txt)},
+	 {0, PATH(32M_10.txt)},
+	 {0, PATH(32M_11.txt)},
+	 {0, PATH(32M_12.txt)},
+	 {0, PATH(32M_13.txt)},
+	 {0, PATH(32M_14.txt)},
+	 {0, PATH(32M_15.txt)},
+	 {0, PATH(32M_16.txt)},
+	 {0, PATH(32M_17.txt)},
+	 {0, PATH(32M_18.txt)},
+	 {0, PATH(32M_19.txt)},
+	 {0, PATH(32M_20.txt)},
+	 {0, PATH(32M_21.txt)},
+	 {0, PATH(32M_22.txt)},
+	 {0, PATH(32M_23.txt)},
+	 {0, PATH(32M_24.txt)},
+	 {0, PATH(32M_25.txt)},
+	 {0, PATH(32M_26.txt)},
+	 {0, PATH(32M_27.txt)},
+	 {0, PATH(32M_28.txt)},
+	 {0, PATH(32M_29.txt)},
+	 {0, PATH(32M_30.txt)},
+	 {0, PATH(32M_31.txt)},
+	 {0, PATH(32M_32.txt)},
+	 {0, PATH(32M_33.txt)},
+	 {0, PATH(32M_34.txt)},
+	 {0, PATH(32M_35.txt)},
+	 {0, PATH(32M_36.txt)},
+	 {0, PATH(32M_37.txt)},
+	 {0, PATH(32M_38.txt)},
+	 {0, PATH(32M_39.txt)},
+	 {0, PATH(32M_40.txt)},
+	 {0, PATH(32M_41.txt)},
+	 {0, PATH(32M_42.txt)},
+	 {0, PATH(32M_43.txt)},
+	 {0, PATH(32M_44.txt)},
+	 {0, PATH(32M_45.txt)},
+	 {0, PATH(32M_46.txt)},
+	 {0, PATH(32M_47.txt)},
+	 {0, PATH(32M_48.txt)},
+	 {0, PATH(32M_49.txt)},
+	 {0, PATH(32M_50.txt)},
+	 {0, NULL},
+};
+
+static file_path file_Iteme[] = { 
+	 {0, PATH(64M_1.txt)},
+	 {0, PATH(64M_2.txt)},
+	 {0, PATH(64M_3.txt)},
+	 {0, PATH(64M_4.txt)},
+	 {0, PATH(64M_5.txt)},
+	 {0, PATH(64M_6.txt)},
+	 {0, PATH(64M_7.txt)},
+	 {0, PATH(64M_8.txt)},
+	 {0, PATH(64M_9.txt)},
+	 {0, PATH(64M_10.txt)},
+	 {0, PATH(64M_11.txt)},
+	 {0, PATH(64M_12.txt)},
+	 {0, PATH(64M_13.txt)},
+	 {0, PATH(64M_14.txt)},
+	 {0, PATH(64M_15.txt)},
+	 {0, PATH(64M_16.txt)},
+	 {0, PATH(64M_17.txt)},
+	 {0, PATH(64M_18.txt)},
+	 {0, PATH(64M_19.txt)},
+	 {0, PATH(64M_20.txt)},
+	 {0, PATH(64M_21.txt)},
+	 {0, PATH(64M_22.txt)},
+	 {0, PATH(64M_23.txt)},
+	 {0, PATH(64M_24.txt)},
+	 {0, PATH(64M_25.txt)},
+	 {0, PATH(64M_26.txt)},
+	 {0, PATH(64M_27.txt)},
+	 {0, PATH(64M_28.txt)},
+	 {0, PATH(64M_29.txt)},
+	 {0, PATH(64M_30.txt)},
+	 {0, PATH(64M_31.txt)},
+	 {0, PATH(64M_32.txt)},
+	 {0, PATH(64M_33.txt)},
+	 {0, PATH(64M_34.txt)},
+	 {0, PATH(64M_35.txt)},
+	 {0, PATH(64M_36.txt)},
+	 {0, PATH(64M_37.txt)},
+	 {0, PATH(64M_38.txt)},
+	 {0, PATH(64M_39.txt)},
+	 {0, PATH(64M_40.txt)},
+	 {0, PATH(64M_41.txt)},
+	 {0, PATH(64M_42.txt)},
+	 {0, PATH(64M_43.txt)},
+	 {0, PATH(64M_44.txt)},
+	 {0, PATH(64M_45.txt)},
+	 {0, PATH(64M_46.txt)},
+	 {0, PATH(64M_47.txt)},
+	 {0, PATH(64M_48.txt)},
+	 {0, PATH(64M_49.txt)},
+	 {0, PATH(64M_50.txt)},
+	 {0, NULL},
+};
+
 file_lst_size_t file_lst[max] = { 
 	 {4194304, file_Itema},
 	 {8388608, file_Itemb},
 	 {16777216, file_Itemc},
+	 {33554432, file_Itemd},
+	 {67108864, file_Iteme},
 };
